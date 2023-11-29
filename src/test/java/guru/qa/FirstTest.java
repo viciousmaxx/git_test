ea494587241558c4ffed8dc05bfa38ed00b1ee59
Max Kamyshanskii <sky_dog@mail.ru>
2023-11-29 20:20:51 +0300

    some change

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -7,12 +7,12 @@ public class FirstTest {
 
     @Test
     void firstTest() {
-        Assertions.assertTrue(3>2);
+        Assertions.assertTrue(3>=2);
     }
 
     @Test
     void secondTest() {
-        Assertions.assertTrue(3>1);
+        Assertions.assertTrue(3>=1);
     }
 
 }
